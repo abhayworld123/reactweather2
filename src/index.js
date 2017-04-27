eabad7b238474146ea39173ed3d25caf34beed18
abhay chaturvedi <abhay chaturvedi>
2017-04-28 00:09:08 +0530

    ko

---
+++ src/index.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var WeatherApp = require('./components/WeatherApp.js');
+import './css/style.css';
+
+
+ReactDOM.render(<WeatherApp />, document.getElementById('root'));
